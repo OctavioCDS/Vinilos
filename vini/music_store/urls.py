6489238043436e888c12cf4bc4c3b5300b505926
Octavio <oc.figueroa@duocuc.cl>
2023-07-02 18:21:53 -0400

    un poco mas responsivo el catalogo

--- vini/music_store/urls.py
+++ vini/music_store/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import Catalogo
+from .views import *
 
 urlpatterns = [
     path('',Catalogo,name='Catalogo'),
+    path('buscador/', buscador, name="buscador"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG == True:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- vini/music_store/views.py
+++ vini/music_store/views.py
@@ -9,3 +9,8 @@ from django.contrib.auth.decorators import login_required
 def Catalogo(request):
     cancion = Cancion.objects.all()
     return render(request,'tienda/catalogo.html',{'cancion': cancion})
+
+def buscador(request):
+    nombre = request.GET.get("buscador")
+    canciones = Cancion.objects.filter(nombre__icontains=nombre)
+    return render(request, 'tienda/catalogo.html', {'cancion': canciones})
